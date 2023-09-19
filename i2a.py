c2114ed7e9f7624ec087ff4547f53c44c2569dd3
thanasis-tzimas <thanasistzimas@protonmail.com>
2023-09-19 22:57:14 +0300

    Improved resizing algorithm by taking into account the aspect ratio.

--- i2a.py
+++ i2a.py
@@ -9,7 +9,14 @@ elif len(argv) > 2:
     print('More than one file were supplied')
     exit(-1)
 
-char_ramp = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'."
+char_ramp = r"$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'."
+
+
+def ratio_fit(width, height, maxw, maxh):
+    ratio = (maxw / width, maxh / height)
+    ratio = min(ratio[0], ratio[1])
+    return (int(width * ratio), int(height * ratio))
+
 
 def normalize(arr):
     arr = arr.astype('float')
@@ -18,21 +25,19 @@ def normalize(arr):
         max = arr[..., i].max()
         if min != max:
             arr[..., i] -= min
-            arr[..., i] *= ((len(char_ramp)-1)/ (max - min))
+            arr[..., i] *= ((len(char_ramp)-1) / (max - min))
     return arr
 
+
 with Image.open(fp=argv[1]) as im:
-    width = 84
-    height = 48
-    im = im.resize(size=(width, height))
+    im = im.resize(size=ratio_fit(im.width, im.height, 84, 48))
     im = im.convert(mode='RGBA')
     arr = np.array(im)
     im = Image.fromarray(normalize(arr).astype('uint8'), 'RGBA')
     im = im.convert(mode='L')
     with open('out.txt', 'w') as out:
-        for h in range(height):
-            for w in range(width):
+        for h in range(im.height):
+            for w in range(im.width):
                 xy = w, h
                 out.write(char_ramp[im.getpixel(xy)])
             out.write('\n')
-                
